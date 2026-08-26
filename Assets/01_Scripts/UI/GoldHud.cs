62471696d70cf597939a7fdb98bcdbecbb899baa
Gachathief <gameya120@gmail.com>
2026-08-26 18:12:29 +0900

    Feat : Gold DOTween
    GoldHud에 DOTween 애니메이션 추가
    - text 사이즈 확대
    - 현재 Money값 만큼 text 변동

--- Assets/01_Scripts/UI/GoldHud.cs
+++ Assets/01_Scripts/UI/GoldHud.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using TMPro;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class GoldHud : MonoBehaviour
     private CurrencySystem currencySystem;
     private TextMeshProUGUI text;
 
+    private Vector3 originScale;
+
     private void Awake()
     {
         currencySystem = FindAnyObjectByType<CurrencySystem>();
@@ -16,13 +19,46 @@ public class GoldHud : MonoBehaviour
     private void OnEnable()
     {
         currencySystem.CurrencyChanged += CurrencySystem_CurrencyChanged;
+        currencySystem.CurrencyChanged += AddGold;
+        currencySystem.CurrencyChanged += AddGoldText;
     }
     private void OnDisable()
     {
         currencySystem.CurrencyChanged -= CurrencySystem_CurrencyChanged;
+        currencySystem.CurrencyChanged -= AddGold;
+        currencySystem.CurrencyChanged -= AddGoldText;
     }
     private void CurrencySystem_CurrencyChanged(int arg1, int arg2)
     {
         text.text = $"Gold : {arg1}";
+    }
+
+    //DOTween Ãß°¡
+    public void AddGold(int arg1, int arg2)
+    {
+        text.transform.DOKill();
+
+        text.transform.localScale = originScale;
+
+        text.transform.DOScale(originScale * 1.2f, 0.12f)
+            .SetEase(Ease.OutBack)
+            .OnComplete(() =>
+            {
+                text.transform.DOScale(originScale, 0.12f).SetEase(Ease.OutQuad);
+            });
+    }
+
+    public void AddGoldText(int targetCoin, int arg2)
+    {
+        int currentGold = currencySystem.Money;
+        int tempGold = currentGold;
+        DOTween.To(() => tempGold, x =>
+        {
+            tempGold = x;
+            text.text = tempGold.ToString();
+        }, targetCoin, 1.0f).OnComplete(() =>
+        {
+            currentGold = targetCoin;
+        });
     }
 }
